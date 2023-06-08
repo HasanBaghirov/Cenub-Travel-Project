cccb2f56597524369dbb3027371e0175f601e694
Class Time <you@example.comclear>
2023-06-08 12:35:18 +0400

    completed update

--- lahiye/travel/assets/app.js
+++ lahiye/travel/assets/app.js
@@ -1,3 +1,26 @@
 $(".your-class").slick({
     dots: false,
 });
+
+
+
+
+const bars=document.querySelector(".hamburger")
+const nav_res=document.querySelector(".nav-res")
+
+
+bars.addEventListener("click",function(){
+    bars.classList.toggle("is-active");
+})
+
+
+const navBars=document.querySelector(".nav-bars")
+
+navBars.addEventListener("click",function(){
+    navBars.classList.toggle("open");
+    nav_res.classList.toggle("nav-res")
+
+    nav_res.style.height.toggle() ="150px";
+   
+
+})
